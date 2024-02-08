aaef63527f6d9163823341c003c36c1048c423bb
phuhuutin <phuhuutin@gmail.com>
2024-02-08 02:29:32 -0800

    Filed AuthProvider

---
+++ src/context/AuthContext.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from 'react';
+
+const AuthContext = React.createContext();
+ 
+export const useAuth = () => useContext(AuthContext);
+
+// AuthProvider component to manage authentication state
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  // Function to handle login
+  const login = (userData) => {
+    // setIsLoggedIn(true);
+    // setUser(userData);
+
+    //testing
+    if(userData.username === 'phuhuutin' && userData.password === '123456'){
+        setIsLoggedIn(true);
+        setUser(userData);
+        console.log("Logined");
+        return true;
+    } else {
+        console.log("Wrong username or password");
+        return false;
+    }
+  };
+
+  // Function to handle logout
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
