03cf6438397999f9fbf50b92b0482ae3cc1c0bc2
phuhuutin <phuhuutin@gmail.com>
2024-02-08 02:29:49 -0800

    Added Basic Login Modal.

---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import {useAuth} from '../context/AuthContext';
+
+export const LoginModal = () => {
+  const { login } = useAuth();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isOpen, setIsLoginOpen] = useState(true);
+  const handleLogin = () => {
+    // Here you would typically perform authentication, for simplicity, I'm just logging in with username
+    if (username.trim() !== '') {
+       // Assuming login() method takes user data
+       const inOrNot = login({username, password });
+       console.log(inOrNot);
+       setIsLoginOpen(!inOrNot); // Close the modal after successful login
+    }
+  };
+
+  return (
+    <div className={`modal ${isOpen ? 'show' : ''}`} tabIndex="-1" style={{ display: isOpen ? 'block' : 'none' }}>
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Login</h5>
+            <button type="button" className="btn-close" aria-label="Close" onClick={()=>setIsLoginOpen(false)}></button>
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" className="form-control" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            {/* <button type="button" className="btn btn-secondary" onClick={onClose}>Close</button> */}
+            <button type="button" className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootswatch/dist/lux/bootstrap.min.css'; // Import Bootswatch theme CSS
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
